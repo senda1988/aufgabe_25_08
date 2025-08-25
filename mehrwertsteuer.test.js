8afde230ebd251bd774112e49ac83ed5c791e991
senda1988 <zidi.senda@gmail.com>
2025-08-25 14:59:14 +0200

    Copy-job blockiert

--- mehrwertsteuer.test.js
+++ mehrwertsteuer.test.js
@@ -1,11 +1,15 @@
 const { mwst, brutto } = require("./mehrwertsteuer.js")
 
 test("7% von 100 gleich 7", () => {
-    expect(mwst(100, 0.07)).toBe(7);
+    expect(mwst(100, 0.07)).toBe(70);
 });
 
 test("19% von 200 gleich 38", () => {
     expect(mwst(200, 0.19)).toBe(38);
+});
+
+test("0 Betrag ergibt 0 Steuer", () => {
+    expect(mwst(0, 0.19)).toBe(0);
 });
 
 
